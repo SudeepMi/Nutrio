ffb2588e4ad8e43c73c841ce419391648b7be0dd
SudeepMI <sudeepm0599@gmail.com>
2022-05-29 18:23:43 +0545

    ..

--- src/Home.js
+++ src/Home.js
@@ -8,24 +8,37 @@ function Home() {
     <div className='row'>
       <div className='col-md-12'>
         <div className='jumbotron text-center'>
-          <img src={logo} className='app-logo img-responsive py-5' alt='logo' />
-          <h3>A Food nutrition app</h3>
-          <div className='apps d-flex justify-content-center'>
-          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-5' to="/blogs" >
+          <div className='row'>
+            <div className='col-lg-5 col-md-8 col-sm-12 m-auto'>
+                <img src={logo} className='app-logo img-responsive py-1' alt='logo' width={'100%'} />
+              </div>
+          </div>
+          <div className='apps d-flex justify-content-center flex-wrap'>
+          <Link className='btn btn-outline-info btn-outline btn-lg px-5 m-4 w-25' to="/blogs" >
           <i className="fab fa-blogger mx-2"></i>
           Blogs</Link>
 
-          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-5' to='/fruits' >
+          <Link className='btn btn-outline-danger btn-outline btn-lg px-5 m-4 w-25' to='/fruits' >
           <i className="fas fa-carrot mx-2"></i>
           Fruity Fruits </Link>
 
-          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-5' to='/food-repo' >
+          <Link className='btn btn-outline-primary btn-outline btn-lg px-5 m-4 w-25' to='/food-repo' >
           <i className="fas fa-utensils mx-2"></i>
           Food Repository </Link>
 
-          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-5' to='/personal-remedies' >
+          <Link className='btn btn-outline-secondary btn-outline btn-lg px-5 m-4 w-25' to='/personal-remedies' >
           <i className="fas fa-seedling mx-2"></i>
           Search Recipes</Link>
+
+          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-4 w-25' to='/pet-foods' >
+          <i className="fas fa-seedling mx-2"></i>
+          Pet Foods</Link>
+
+          <Link className='btn btn-outline-dark btn-outline btn-lg px-5 m-4 w-25' to='/pet-foods' >
+          <i className="fas fa-seedling mx-2"></i>
+          Pet Foods</Link>
+
+          
           </div>
       </div></div></div>
   </div>
